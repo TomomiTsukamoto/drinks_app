63032771fa145316be730d85b3b8c7f35149ccf2
Tomomi Tsukamoto <127223151+TomomiTsukamoto@users.noreply.github.com>
2023-07-28 01:56:18 +0900

    156

--- src/drink.py
+++ src/drink.py
@@ -1,28 +1,28 @@
-# -*- coding: utf-8 -*-
 # 必要なモジュールのインポート
 from torchvision import transforms
 import pytorch_lightning as pl
 import torch.nn as nn
-# 学習時に使用したものと同じ学習済みモデルをインポート
-from torchvision.models import resnet18
+#学習時に使ったのと同じ学習済みモデルをインポート
+from torchvision.models import resnet18 
 
-
-#前処理
-transforms  = transforms.Compose({
-    transforms.ToTensor(), 
-    transforms.Resize((224, 224))
+# 学習済みモデルに合わせた前処理を追加
+transforms = transforms.Compose({
+    transforms.ToTensor(),
+    transforms.Resize((224,224)),
 })
 
+#　ネットワークの定義
 class Net(pl.LightningModule):
 
-    def __init__(self): 
+    def __init__(self):
         super().__init__()
 
-        self.feature = resnet18(pretrained=True)
+        #学習時に使ったのと同じ学習済みモデルを定義
+        self.feature = resnet18(pretrained=True) 
         self.fc = nn.Linear(1000, 2)
 
-
     def forward(self, x):
+        #学習時に使ったのと同じ順伝播
         h = self.feature(x)
         h = self.fc(h)
         return h
